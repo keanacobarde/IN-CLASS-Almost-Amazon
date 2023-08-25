28ac6094fc7f51cc098a9827f1ed4c8023a3b3b9
Keana Cobarde <keanacobarde@gmail.com>
2023-08-25 14:47:24 -0500

    created author details card. coloring and styling is still off but functionality and base structure is present

--- pages/viewAuthor.js
+++ pages/viewAuthor.js
@@ -18,13 +18,14 @@ const viewAuthor = (obj) => {
     domString += `<div class="card mb-3" style="max-width: 540px;">
     <div class="row g-0">
       <div class="col-md-4">
-        <img src="..." class="img-fluid rounded-start" alt="...">
+        <img src="${obj.bookObject[i].image}" class="img-fluid rounded-start" alt="...">
       </div>
       <div class="col-md-8">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+          <h5 class="card-title">${obj.bookObject[i].title}</h5>
+          <p class="card-text">${obj.bookObject[i].description}</p>
+          <p class="card-text"><small class="text-body-secondary"><p>${obj.bookObject[i].sale ? `<span class="badge bg-info sale-badge"><i class="fa fa-bell" aria-hidden="true"></i> Sale</span> 
+          $${obj.bookObject[i].price}` : `$${obj.bookObject[i].price}`}</p> </small></p>
         </div>
       </div>
     </div>
